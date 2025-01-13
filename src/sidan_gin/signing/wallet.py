c3ab12ef8a5e4c553441c289c6da8fa1da4e2d1a
twwu123 <tszwaiwu.96@gmail.com>
2025-01-13 14:54:26 +0800

    implement signing tx

--- src/sidan_gin/signing/wallet.py
+++ src/sidan_gin/signing/wallet.py
@@ -12,10 +12,20 @@ class HDWallet:
 
     def sign_tx(self, tx_hex):
         raw_decoded_cbor = loads(bytes.fromhex(tx_hex))
-        return self.sign(blake2b(dumps(raw_decoded_cbor[0]), 32, encoder=RawEncoder))
+        raw_tx_body = raw_decoded_cbor[0]
+        signature = self.sign(blake2b(dumps(raw_tx_body), 32, encoder=RawEncoder))
+        raw_witness_set = raw_decoded_cbor[1]
+        if (0 in raw_witness_set):
+            raw_vkeys = raw_witness_set[0]
+            raw_vkeys.append([self.verification_key.to_non_extended().to_cbor(), signature])
+            raw_witness_set[0] = raw_vkeys
+        else:
+            raw_witness_set[0] = [[self.verification_key.to_non_extended().to_cbor()[2::], signature]]
+        raw_decoded_cbor[1] = raw_witness_set
+        return dumps(raw_decoded_cbor).hex()
 
     def sign_message_hex(self, message_hex):
         return self.sign(bytes.fromhex(message_hex))
 
     def sign(self, message):
-        return self.signing_key.sign(message).hex()
+        return self.signing_key.sign(message)
